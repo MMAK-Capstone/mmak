ed15a4db4e4397d13b21e04e369ef3f34bb1847f
Keyairra Wright <keyairra.wright@gmail.com>
2018-10-01 17:31:13 -0400

    installing necessary packages to dev branch

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -1,10 +1,3 @@
-<<<<<<< navbar
-import React, {Component} from 'react';
-import {Link, withRouter} from 'react-router-dom';
-import Button from '@material-ui/core/Button'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-=======
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
@@ -13,7 +6,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
->>>>>>> dev
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
@@ -23,7 +15,6 @@ import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import Avatar from '@material-ui/core/Avatar';
 
-<<<<<<< navbar
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -105,102 +96,6 @@ class Homepage extends Component {
     </div>
     );
   }
-=======
-const styles = (theme) => ({
-	root: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'space-around',
-		overflow: 'hidden',
-		backgroundColor: theme.palette.background.paper
-	},
-	gridList: {
-		width: 1000,
-		height: 'auto',
-		overflowY: 'auto'
-	},
-	title: {
-		color: theme.palette.primary.light
-	},
-	titleBar: {
-		background: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
-	},
-	indvCell: {
-		borderRadius: 45
-	}
-});
-
-const we = [
-	{
-		img: '/pictures/Michelle.JPG',
-		title: 'Michelle',
-		description: 'ansakkk'
-	},
-	{
-		img: '/pictures/Megha.jpg',
-		title: 'Megha',
-		description: 'kjkjdkjsk'
-	},
-	{
-		img: '/pictures/Asya.jpg',
-		title: 'Asya',
-		description: 'jhjhjhjk'
-	},
-	{
-		img: '/pictures/Keyairra.png',
-		title: 'Keyairra',
-		description: 'djshdshdjk'
-	}
-];
-
-class Homepage extends Component {
-	render() {
-		return (
-			<div>
-				<AppBar position="static">
-					<Toolbar>
-						<Typography variant="title" color="inherit">
-							NiceLogo
-						</Typography>
-						<Button>Login</Button>
-						{/* <Button>Sign Up</Button> */}
-						<Link to="/signup">Sign Up</Link>
-					</Toolbar>
-				</AppBar>
-				<Tabs>
-					<Tab label="Education Game">
-						<div>
-							<h2>Education Game</h2>
-						</div>
-					</Tab>
-					<Tab label="Fun Game">
-						<div>
-							<h2>Fun Game</h2>
-						</div>
-					</Tab>
-				</Tabs>
-				<div style={styles.root}>
-					<GridList cellHeight={100} style={styles.gridList} cols={2}>
-						{we.map((tile) => (
-							<GridListTile key={tile.img} style={styles.indvCell}>
-								<Avatar alt={tile.title} src={tile.img} />
-								<GridListTileBar
-									title={tile.title}
-									subtitle={tile.description}
-									actionIcon={
-										<IconButton>
-											<StarBorderIcon />
-										</IconButton>
-									}
-								/>
-							</GridListTile>
-						))}
-					</GridList>
-				</div>
-			</div>
-		);
-	}
->>>>>>> dev
 }
 
 export default withStyles(styles)(Homepage);
