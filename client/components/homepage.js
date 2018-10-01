e8c8e7838ead09ebaf4ff07eef9f8a5c7b0525bf
AsyaSlutskaya <asyaslutskaya@gmail.com>
2018-10-01 16:57:16 -0400

    homepage and navbar

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
 import {Link, withRouter} from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
@@ -15,8 +12,6 @@ import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import Avatar from '@material-ui/core/Avatar';
 
-
-
 const styles = theme => ({
   root: {
     display: 'flex',
@@ -27,7 +22,6 @@ const styles = theme => ({
   },
   gridList: {
     "width": 1000,
-    "height": 'auto',
     "overflowY": 'auto',
   },
   title: {
@@ -46,36 +40,33 @@ const we = [
   {
     img: '/pictures/Michelle.JPG',
     title: 'Michelle',
-    description: "ansakkk"
+    description: "Michelle is a Fullstack Engineer who would enjoy working someplace that loves paying it forward to their communities",
+    linkedin: "https://www.linkedin.com/in/michellemessenger/",
   },
   {
     img: '/pictures/Megha.jpg',
     title: 'Megha',
-    description: "kjkjdkjsk"
+    description: "Megha is an Energetic and Passionate Fullstack developer able to build a Web presence from the ground up from concept, navigation, layout and programming. Would like to work with Software Development Co. for front-end and back-end",
+    linkedin:"https://www.linkedin.com/in/megha-25/"
   },
   {
     img: '/pictures/Asya.jpg',
     title: 'Asya',
-    description:"jhjhjhjk"
+    description:"Asya would love to work in FinTech and just to work :)",
+    linkedin: "https://www.linkedin.com/in/asya-slutskaya-978242141/"
   },
   {
     img: '/pictures/Keyairra.png',
     title: 'Keyairra',
-    description:"djshdshdjk"
+    description:"Keyairra is a Fullstack Javascript Developer who is interested in EdTech, Gaming, and Social Impact companies",
+    linkedin: "https://www.linkedin.com/in/keyairra-s-wright/"
   },
 ];
 
 class Homepage extends Component {
   render(){
     return (
-    <div>   
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="title" color="inherit">NiceLogo</Typography>
-        <Button>Login</Button>
-        <Button>Sign Up</Button>
-      </Toolbar>
-    </AppBar>
+    <div>
         <Tabs>
           <Tab label="Education Game">
             <div>
@@ -89,12 +80,12 @@ class Homepage extends Component {
           </Tab>
         </Tabs>
           <div style={styles.root}>
-            <GridList cellHeight={100} style={styles.gridList} cols={2}>
+            <GridList cellHeight={110} style={styles.gridList} cols={4}>
               {we.map((tile) => (
-                <GridListTile key={tile.img} style={styles.indvCell}>
-                  <Avatar alt={tile.title} src={tile.img}/>
+                <GridListTile key={tile.img} style={{flexGrow: 2}}>
+                  <Avatar src={tile.img}/>
                   <GridListTileBar title={tile.title} subtitle={tile.description} actionIcon={
-                    <IconButton><StarBorderIcon/></IconButton>
+                    <a href={tile.linkedin}><IconButton><StarBorderIcon/></IconButton></a>
                   }/></GridListTile>
               ))}
             </GridList>
